4f188ed20c46cdaf9445ebc2058d52edbdccc980
ekhassan <110443857+ekhassan@users.noreply.github.com>
2023-01-08 23:16:59 +0500

    push

--- src/JarvisX/DataBase.java
+++ src/JarvisX/DataBase.java
@@ -17,36 +17,36 @@ public class DataBase {
     public static String g;
     public static String h;
 
-    public  String[] dbprojects=new String[100];
-    public String[] dbemps=new String[100];
-    public static String[] dbemppros=new String[100];
-    public static String[] dbclpros=new String[100];
+    public String[] dbprojects = new String[100];
+    public String[] dbemps = new String[100];
+    public static String[] dbemppros = new String[100];
+    public static String[] dbclpros = new String[100];
 
-    public void showproDetails(String proname){
+    public void showproDetails(String proname) {
 
         PreparedStatement statement;
         ResultSet rs;
 
         try {
-            connection =DriverManager.getConnection(databaseURL);
+            connection = DriverManager.getConnection(databaseURL);
             String sql = "SELECT * FROM Project";
             statement = connection.prepareStatement(sql);
             rs = statement.executeQuery();
 
-            while(rs.next()) {
-                String name=rs.getString(3);
+            while (rs.next()) {
+                String name = rs.getString(3);
 
-                if(name.equals(proname)){
+                if (name.equals(proname)) {
 
 
-                    a=rs.getString("proid");
-                    b=rs.getString("proname");
-                    c=rs.getString("prosource");
-                    d=rs.getString("profrom");
-                    e=rs.getString("proto");
-                    f=rs.getString("prostatus");
-                    g=rs.getString("prorev");
-                    h=rs.getString("prorate");
+                    a = rs.getString("proid");
+                    b = rs.getString("proname");
+                    c = rs.getString("prosource");
+                    d = rs.getString("profrom");
+                    e = rs.getString("proto");
+                    f = rs.getString("prostatus");
+                    g = rs.getString("prorev");
+                    h = rs.getString("prorate");
 
 
                 }
@@ -61,26 +61,25 @@ public class DataBase {
     public void showProjects() {
         PreparedStatement statement;
         ResultSet rs;
-        int i=0;
-        int a=0;
+        int i = 0;
+        int a = 0;
         try {
-            connection =DriverManager.getConnection(databaseURL);
+            connection = DriverManager.getConnection(databaseURL);
             String sql = "SELECT * FROM Project";
             statement = connection.prepareStatement(sql);
             rs = statement.executeQuery();
 
-            while(rs.next()) {
-                for (int x=0; x<100; x++) {
-                    if(dbprojects[x]==rs.getString("proname")) {
-                        i=0;
+            while (rs.next()) {
+                for (int x = 0; x < 100; x++) {
+                    if (dbprojects[x] == rs.getString("proname")) {
+                        i = 0;
                         break;
-                    }
-                    else {
-                        i=1;
+                    } else {
+                        i = 1;
                     }
                 }
-                if(i==1) {
-                    dbprojects[a]=rs.getString("proname");
+                if (i == 1) {
+                    dbprojects[a] = rs.getString("proname");
                 }
                 ++a;
             }
@@ -93,26 +92,25 @@ public class DataBase {
     public void showEmps() {
         PreparedStatement statement;
         ResultSet rs;
-        int i=0;
-        int a=0;
+        int i = 0;
+        int a = 0;
         try {
-            connection =DriverManager.getConnection(databaseURL);
+            connection = DriverManager.getConnection(databaseURL);
             String sql = "SELECT * FROM Employee";
             statement = connection.prepareStatement(sql);
             rs = statement.executeQuery();
 
-            while(rs.next()) {
-                for (int x=0; x<100; x++) {
-                    if(dbemps[x]==rs.getString("name")) {
-                        i=0;
+            while (rs.next()) {
+                for (int x = 0; x < 100; x++) {
+                    if (dbemps[x] == rs.getString("name")) {
+                        i = 0;
                         break;
-                    }
-                    else {
-                        i=1;
+                    } else {
+                        i = 1;
                     }
                 }
-                if(i==1) {
-                    dbemps[a]=rs.getString("name");
+                if (i == 1) {
+                    dbemps[a] = rs.getString("name");
                 }
                 ++a;
             }
@@ -121,7 +119,6 @@ public class DataBase {
             e.printStackTrace();
         }
     }
-
 
 
     public void AssignTo(String prname, String assignedTo) {
@@ -137,11 +134,11 @@ public class DataBase {
             statementC = connection.prepareStatement(sqlC);
             rs = statementC.executeQuery();
 
-            while(rs.next()) {
-                if(prname.equals(rs.getString("proname"))) {
-                    idNo= rs.getString("id");
-                    sql = "UPDATE Project"+
-                            " SET proto = '"+assignedTo+"' WHERE id in ("+idNo+")";
+            while (rs.next()) {
+                if (prname.equals(rs.getString("proname"))) {
+                    idNo = rs.getString("id");
+                    sql = "UPDATE Project" +
+                            " SET proto = '" + assignedTo + "' WHERE id in (" + idNo + ")";
                     statement = connection.prepareStatement(sql);
                     statement.executeUpdate();
                     statement.close();
@@ -168,11 +165,11 @@ public class DataBase {
             statementC = connection.prepareStatement(sqlC);
             rs = statementC.executeQuery();
 
-            while(rs.next()) {
-                if(prname.equals(rs.getString("proname"))) {
-                    idNo= rs.getString("id");
-                    sql = "UPDATE Project"+
-                            " SET prostatus = '"+status+"' WHERE id in ("+idNo+")";
+            while (rs.next()) {
+                if (prname.equals(rs.getString("proname"))) {
+                    idNo = rs.getString("id");
+                    sql = "UPDATE Project" +
+                            " SET prostatus = '" + status + "' WHERE id in (" + idNo + ")";
                     statement = connection.prepareStatement(sql);
                     statement.executeUpdate();
                     statement.close();
@@ -199,11 +196,11 @@ public class DataBase {
             statementC = connection.prepareStatement(sqlC);
             rs = statementC.executeQuery();
 
-            while(rs.next()) {
-                if(prname.equals(rs.getString("proname"))) {
-                    idNo= rs.getString("id");
-                    sql = "UPDATE Project"+
-                            " SET prorate = '"+rate+"' WHERE id in ("+idNo+")";
+            while (rs.next()) {
+                if (prname.equals(rs.getString("proname"))) {
+                    idNo = rs.getString("id");
+                    sql = "UPDATE Project" +
+                            " SET prorate = '" + rate + "' WHERE id in (" + idNo + ")";
                     statement = connection.prepareStatement(sql);
                     statement.executeUpdate();
                     statement.close();
@@ -216,6 +213,7 @@ public class DataBase {
             e.printStackTrace();
         }
     }
+
     public void rev(String prname, String rev) {
         PreparedStatement statementC;
         PreparedStatement statement;
@@ -229,11 +227,11 @@ public class DataBase {
             statementC = connection.prepareStatement(sqlC);
             rs = statementC.executeQuery();
 
-            while(rs.next()) {
-                if(prname.equals(rs.getString("proname"))) {
-                    idNo= rs.getString("id");
-                    sql = "UPDATE Project"+
-                            " SET prorev = '"+rev+"' WHERE id in ("+idNo+")";
+            while (rs.next()) {
+                if (prname.equals(rs.getString("proname"))) {
+                    idNo = rs.getString("id");
+                    sql = "UPDATE Project" +
+                            " SET prorev = '" + rev + "' WHERE id in (" + idNo + ")";
                     statement = connection.prepareStatement(sql);
                     statement.executeUpdate();
                     statement.close();
@@ -246,6 +244,7 @@ public class DataBase {
             e.printStackTrace();
         }
     }
+
     public void status(String prname) {
         PreparedStatement statementC;
         PreparedStatement statement;
@@ -259,11 +258,11 @@ public class DataBase {
             statementC = connection.prepareStatement(sqlC);
             rs = statementC.executeQuery();
 
-            while(rs.next()) {
-                if(prname.equals(rs.getString("proname"))) {
-                    idNo= rs.getString("id");
-                    sql = "UPDATE Project"+
-                            " SET prostatus = 'Closed' WHERE id in ("+idNo+")";
+            while (rs.next()) {
+                if (prname.equals(rs.getString("proname"))) {
+                    idNo = rs.getString("id");
+                    sql = "UPDATE Project" +
+                            " SET prostatus = 'Closed' WHERE id in (" + idNo + ")";
                     statement = connection.prepareStatement(sql);
                     statement.executeUpdate();
                     statement.close();
@@ -276,9 +275,10 @@ public class DataBase {
             e.printStackTrace();
         }
     }
+
     public void addDBPro(String a, String b, String c, String d, String e) {
         try {
-            connection =DriverManager.getConnection(databaseURL);
+            connection = DriverManager.getConnection(databaseURL);
 
             String sql;
             sql = "INSERT INTO Project (proid, proname, prosource, profrom, proto, prostatus, prorev, prorate) VALUES (?,?,?,?,?,?,?,?)";
@@ -304,7 +304,7 @@ public class DataBase {
 
     public void addDBEmp(String u, String p, String n) {
         try {
-            connection =DriverManager.getConnection(databaseURL);
+            connection = DriverManager.getConnection(databaseURL);
 
             String sql;
             sql = "INSERT INTO Employee (username, password, name) VALUES (?,?,?)";
@@ -325,6 +325,127 @@ public class DataBase {
         }
     }
 
+    public void addDBCl(String u, String p, String n) {
+        try {
+            connection = DriverManager.getConnection(databaseURL);
+
+            String sql;
+            sql = "INSERT INTO Client (username, password, name) VALUES (?,?,?)";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, u);
+            statement.setString(2, p);
+
+            statement.setString(3, n);
 
 
+            statement.executeUpdate();
+            statement.close();
+            connection.close();
+
+        } catch (SQLException r) {
+            r.printStackTrace();
+        }
+    }
+
+    public void delDBEmp(String emp) {
+        PreparedStatement statement;
+        PreparedStatement statementC;
+        String idNo;
+        ResultSet rs;
+        String sql;
+        String sqlC;
+        try {
+            connection = DriverManager.getConnection(databaseURL);
+            sqlC = "SELECT * FROM Employee";
+            statementC = connection.prepareStatement(sqlC);
+            rs = statementC.executeQuery();
+
+            while (rs.next()) {
+                if (emp.equals(rs.getString("name"))) {
+                    idNo = rs.getString("ID");
+                    sql = "DELETE FROM Employee WHERE id in (" + idNo + ")";
+                    statement = connection.prepareStatement(sql);
+                    statement.executeUpdate();
+                    statement.close();
+                    break;
+                }
+            }
+            statementC.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showEmpPro(String emp) {
+        PreparedStatement statement;
+        ResultSet rs;
+        int i = 0;
+        int a = 0;
+        try {
+            connection = DriverManager.getConnection(databaseURL);
+            String sql = "SELECT * FROM Project";
+            statement = connection.prepareStatement(sql);
+            rs = statement.executeQuery();
+
+            while (rs.next()) {
+                String uname = rs.getString(6);
+                if (uname.equals(emp)) {
+                    for (int x = 0; x < 100; x++) {
+                        if (dbemppros[x] == rs.getString("proname")) {
+                            i = 0;
+                            break;
+                        } else {
+                            i = 1;
+                        }
+                    }
+                    if (i == 1) {
+                        dbemppros[a] = rs.getString("proname");
+                    }
+                    ++a;
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showClPro(String cli) {
+        PreparedStatement statement;
+        ResultSet rs;
+        int i = 0;
+        int a = 0;
+        try {
+            connection = DriverManager.getConnection(databaseURL);
+            String sql = "SELECT * FROM Project";
+            statement = connection.prepareStatement(sql);
+            rs = statement.executeQuery();
+
+            while (rs.next()) {
+                String uname = rs.getString(5);
+                if (uname.equals(cli)) {
+                    for (int x = 0; x < 100; x++) {
+                        if (dbclpros[x] == rs.getString("proname")) {
+                            i = 0;
+                            break;
+                        } else {
+                            i = 1;
+                        }
+                    }
+                    if (i == 1) {
+                        dbclpros[a] = rs.getString("proname");
+                    }
+                    ++a;
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
